c1652ceebd632b51e0ee197e7a1addffd184243d
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-03-02 21:24:16 -0300

    atualizando diretorio com retornos fakes para funcionamento do laço de repetição

--- classes/Diretorio.php
+++ classes/Diretorio.php
@@ -2,18 +2,33 @@
 
 interface DiretorioInterface
 {
-    public function recuperarListaArquivos(){}
-    public function excluirArquivo($nome_arquivo){}
-    public function cadastrarArquivo($arquivo){}
-    /**
-     * se precisra pode construir mais métodos 
-     * ou até mesmo dar manutenção nesta interface
-     */
+    public function recuperarListaArquivos();
+    public function excluirArquivo($nome_arquivo);
+    public function cadastrarArquivo($arquivo);
 }
 
 class Diretorio implements DiretorioInterface
 {
+    public function recuperarListaArquivos()
+    {
+    	// este código precisa se tornar dinâmico
+	    // com base nos arquivos que a pasta uploads possui
+	    // não deixar assim
+    	$array = array('image1.jpg', 'image2.jpg', 'arquivo_nao_existe.jpg');
 
+	    return $array;
+    }
+
+    public function excluirArquivo($nome_arquivo)
+    {
+    	// o retorno true deverá ser feito caso o arquivo seja excluído
+		return true;
+    }
+
+    public function cadastrarArquivo($arquivo)
+    {
+    	// criar código
+    }
 }
 
 ?>
